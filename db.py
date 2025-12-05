ac5245ad4b76f61900d57754c7df15f94a2186ab
jjcamacho2905 <jjcamacho29@ucatolica.edu.co>
2025-12-05 09:27:06 -0500

    implementacion en db
    Added database connection setup and session management.

--- db.py
+++ db.py
@@ -1,1 +1,25 @@
+from sqlmodel import SQLModel, create_engine, Session
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL no está definida")
+
+
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,
+)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
