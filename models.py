95a720341085aab880fce600bd3ad78d91137a6d
Maria Rincón <majorincon2109@gmail.com>
2025-12-05 10:54:58 -0500

    errores
    Removed unnecessary comments from field definitions.

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from utils.positions import Position
 from utils.states import States
 
 class JugadorEstadisticasLink(SQLModel, table=True):
-    jugador_dorsal: Optional[int] = Field(default=None, foreign_key="jugador.dorsal", primary_key=True)
+    jugador_dorsal: Optional[int] = Field(default=None, foreign_key="jugador.dorsal", primary_key=True) ##
     estadistica_id: Optional[int] = Field(default=None, foreign_key="estadistica.id", primary_key=True)
 
 class JugadorPosicionLink(SQLModel, table=True):
@@ -14,7 +14,7 @@ class JugadorPosicionLink(SQLModel, table=True):
     posicion: Optional[int] = Field(default=None, foreign_key="position.", primary_key=True)
 
 class JugadorBase(SQLModel):
-    dorsal[int] = Field(default=None, primary_key=True)
+    dorsal[int] = Field(default=None, primary_key=True)   ##
     nombre: Optional[str]=None
     altura: Optional[float]=None
     peso: Optional[float]=None
