0dcbbd230e4fd45ce39106957672e902a3025733
Maria Rincón <majorincon2109@gmail.com>
2025-12-05 10:03:40 -0500

    correcion de models.py

--- models.py
+++ models.py
@@ -1,37 +1,48 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Float, DateTime, Enum as SQLEnum, func
-from sqlalchemy.orm import relationship
-from database import Base
-from enum import Enum
+from dataclasses import field
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List
+from datetime import date
+from utils.positions import Position
+from utils.states import States
+
+class JugadorEstadisticasLink(SQLModel, table=True):
+    jugador_dorsal: Optional[int] = Field(default=None, foreign_key="jugador.dorsal", primary_key=True)
+    estadistica_id: Optional[int] = Field(default=None, foreign_key="estadistica.id", primary_key=True)
+
+class JugadorPosicionLink(SQLModel, table=True):
+    jugador_dorsal: Optional[int] = Field(default=None, foreign_key="jugador.dorsal", primary_key=True)
+    posicion: Optional[int] = Field(default=None, foreign_key="position.", primary_key=True)
+
+class JugadorBase(SQLModel):
+    dorsal[int] = Field(default=None, primary_key=True)
+    nombre: Optional[str]=None
+    altura: Optional[float]=None
+    peso: Optional[float]=None
+    año_nacimiento: Optional[date]=None
+    pie_dominante: Optional[str]=None
+    tiempo_cancha: Optional[int]=None
+    goles: Optional[int]=None
+    faltas: Optional[int]=None
+    fecha_nacimiento: Optional[date]=None
+
+    
+
+class Jugador(JugadorBase, table=True):
+    dorsal:Optional[str]=None
+    nombre = Optional[str]=None
+    active: bool = Field(default=True)
+    estadisticas: List[Estadistica] = Relationship(back_populates="jugador", link_model=JugadorEstadisticasLink)
+    posición: List[str]= Relationship(back_populates="")
+
+class EstadisticaBase(SQLModel):
+    goles: Optional[int]=None
+    faltas: Optional[int]=None
+    tiempo_de_juego:[int]=None
 
 
-
-class Jugador(Base):
-    """Modelo de Jugador"""
-    __tablename__ = "jugadores"
-
-    id = Column(Integer, primary_key=True, index=True)
-    nombre = Column(String, nullable=False, index=True)
-    edad = Column(Integer, nullable=False)
-    posicion = Column(SQLEnum(Position), nullable=False)
-    numero = Column(Integer, nullable=False, unique=True)  # Número de camiseta único
-    estado = Column(SQLEnum(States), default=States.ACTIVO)
-    foto_path = Column(String, nullable=True)
-    fecha_registro = Column(DateTime, server_default=func.now())
-    activo = Column(Boolean, default=True)
-
-    pass
-
-
-class Estadistica():
-    pass
+class Estadistica(EstadisticaBase, table=True):
+    id: Optional[int]
 
 
 class Partido():
     pass
-
- # Estadísticas del jugador en ese partido específico
-    goles_partido = Column(Integer, default=0)
-    asistencias_partido = Column(Integer, default=0)
-    minutos_jugados = Column(Integer, default=0)
-    tarjeta_amarilla = Column(Boolean, default=False)
-    tarjeta_roja = Column(Boolean, default=False)
